6ecccdb9e9c994d42e6d7117ace943deec7ef2d1
Mehul Patel <mehulpatel696@gmail.com>
2015-10-10 23:23:20 +0400

    set up main

---
+++ largestRectangle.cpp
@@ -0,0 +1,24 @@
+nclude <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+int calculate_area(stack<int>* buildings){
+    
+}
+
+int main() {
+    stack<int> buildings;
+    int num_buildings;
+    cin>>num_buildings;
+    while(num_buildings > 0){
+    	int height; 
+	cin>>height;
+	
+    }
+    return 0;
+}
+
