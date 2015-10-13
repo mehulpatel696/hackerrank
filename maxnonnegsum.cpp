a6d8dcc084e9a1db1856b2242e0c39ba8aa0a745
Mehul Patel <mehulpatel696@gmail.com>
2015-10-13 18:03:02 +0400

    Find Max Postive Slice works

---
+++ maxnonnegsum.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxSlice(vector<int>& A){
+    if(A.size() < 2) return -1;
+    int currentSum = 0;
+    int maxSum = 0;
+    for(int i = 0; i < A.size(); i++){
+        if(A[i] >= 0) {
+            currentSum += A[i];
+            if(maxSum < currentSum) maxSum = currentSum;
+        }
+        else currentSum = 0;
+    }
+    if(maxSum == 0) return -1;
+    return maxSum;
+}
+
+int main(){
+    //Tester Code
+    vector<int> A;
+    A.push_back(-1);
+    A.push_back(-1);
+    A.push_back(-1);
+    A.push_back(-1);
+    A.push_back(-1);
+    A.push_back(-1);
+    A.push_back(-1);
+    
+    
+    
+    cout<<maxSlice(A)<<endl;
+}
