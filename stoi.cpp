b7eead2583dd4ce1bca85de358eb3cedd98f461b
Mehul Patel <mehulpatel696@gmail.com>
2015-10-13 17:43:16 +0400

    fixed a bug in stoi

--- stoi.cpp
+++ stoi.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
 #include <string>
 #include <math.h>
-using namespace std; 
+using namespace std;
 
 signed long long parseLong(string str){
-  //use ASCII 48 - (int) char; 
-  int hasSign = 0;
-  int size = str.size() - 1;
-  if((int) str[0] == 43 || (int) str[0] == 45){
-  	hasSign = 1;
-	size--;
-  }
-  int number = 0; 
-  for(int i = hasSign; i < str.size(); i++){
-     if( (int) str[i] > 57 || (int) str[i] < 48 ) return -1;
-     number += (pow(10,size)) * ( 48 - (int) str[i]);
-     size--;
-  }
-  if((int) str[0] == 45) return number * -1; 
-  return number;
-} 
+    //use ASCII 48 - (int) char;
+    int hasSign = 0;
+    int size = str.size() - 1;
+    if((int) str[0] == 43 || (int) str[0] == 45){
+        hasSign = 1;
+        size--;
+    }
+    signed long long number = 0;
+    for(int i = hasSign; i < str.size() ; i++){
+        if( (int) str[i] > 57 || (int) str[i] < 48 ) return -1;
+        number += (pow(10,size)) * ((int) str[i] - 48);
+        size--;
+    }
+    if( (int) str[0] == 45) return number * -1;
+    return number;
+}
+
 int main(){
-  cout<<parseLong("123")<<endl;
-  cout<<parseLong("11111123")<<endl;
-  cout<<parseLong("-123")<<endl;
-  cout<<parseLong("1ff23")<<endl;
-  cout<<parseLong("11111111111111123")<<endl;
-  cout<<parseLong("8901a")<<endl;
-  return 0;
+    cout<<parseLong("123")<<endl;
+    cout<<parseLong("11111123")<<endl;
+    cout<<parseLong("-123")<<endl;
+    cout<<parseLong("1ff23")<<endl;
+    cout<<parseLong("111111111126")<<endl;
+    cout<<parseLong("8901a")<<endl;
+    return 0;
 }
