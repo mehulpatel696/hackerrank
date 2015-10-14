7188c1666f8bffe85bc6cab5a552e94059676c5e
Mehul Patel <mehulpatel696@gmail.com>
2015-10-14 12:53:26 +0400

    Removed extra files

--- maximisesum.cpp
+++ maximisesum.cpp
@@ -1,41 +1,41 @@
-#include <iostream>
+#include <cmath>
+#include <cstdio>
 #include <vector>
-using namespace std; 
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
 
-
-int findMaxSum(vector<int> numbers, long long int  M){ 
-    //Kepp track of maxSum with a var 
-    //Loop through the array and find sume and it's mod and compare to maxSum 
+int findMaxSum(vector<int> numbers, int  M){ 
+    
     int maxModSum = 0; 
     int currentSum = 0; 
     for(int i = 0; i < numbers.size(); i++){
       for(int j = i; j < numbers.size(); j++){
         currentSum += numbers[j];
-	cout<<currentSum<<endl;
-	if(maxModSum < (currentSum%M)) maxModSum = (currentSum%M);
+        if(maxModSum < (currentSum%M)) maxModSum = (currentSum%M);
       }
+      currentSum = 0;
     }
-    currentSum = 0;
     return maxModSum; 
-
-
+    
 }
+
 int main(){
-	int N;
-	long long int M; 
-	cin>>N;
-	cout<<" ";
-	cin>>M;
-	cout<<endl;
-	vector<int> numbers; 
-	for(int i = 0; i < N; i++){
-	    int tmp; 
-	    cin>>tmp;
-	    cout<<" ";
-	    numbers.push_back(tmp);
-	    if( i ==  (N-1)) cout<<endl;
-	}
-	cout<<findMaxSum(numbers, M);
+    int numTestCases; 
+    cin>> numTestCases;
+    for( int j = 0; j < numTestCases; j++){
+        int N;
+        int M; 
+        cin>> N >> M;
+        vector<int> numbers; 
+        for(int i = 0; i < N; i++){
+            int tmp; 
+            cin>>tmp;
+            numbers.push_back(tmp);
+        }
+        cout<<findMaxSum(numbers, M);
+    }
+
 	return 0;
 }
