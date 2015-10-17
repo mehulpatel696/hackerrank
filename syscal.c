b0e793d47200b9ab30f6c95fa712efb667648903
Mehul Patel <mehulpatel696@gmail.com>
2015-10-17 11:59:47 +0400

    Implemented a System Call timer

---
+++ syscal.c
@@ -0,0 +1,19 @@
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+
+//#define
+#ifndef NOSYSCALL
+int getPid() {return 55;}
+#endif 
+ int main( int argc, char * argv[] ) {
+ int i, a, limit = atoi(argv[1]);
+ clock_t start, end;
+ start = clock();
+ for( i = 0; i < limit; ++i ){
+	 a = getPid();
+ }
+ end = clock() - start;
+ printf("Seconds: %f seconds.\n",(float)end/CLOCKS_PER_SEC);
+}
