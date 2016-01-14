ab927c6bbed3f9a22011402547d6eab6b4fc484e
Mehul Patel <mehulpatel696@gmail.com>
2016-01-14 14:05:11 -0500

    final

--- bug.cpp
+++ bug.cpp
@@ -1,2 +1,22 @@
+#include <iostream> 
+#include <vector> 
+#include <string>
 
-     int c = 0; 
+
+void printMatrix(int array[8][11]){
+  for(int i = 0; i < 8; i++){
+      for(int j = 0; j < 11; j++) {
+          cout<<array[i][j];
+       }
+      cout<<endl;
+  }
+}
+
+int main(){
+  string welcome = "Hello, please enter the 8*11 2d array that you would like printed"; 
+  cout<<string; 
+  int array[8][11];
+  for(int i = 0; i < 8; i++) for(int j = 0; j < 11; j++) cin>>array[i][j];
+  printMatrix(array);
+  return 0;
+}
